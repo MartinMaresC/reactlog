277bc4ccddb196573bf2614ce798ddde6186f1a9
Martin <martinmarescr@gmail.com>
2022-12-24 11:31:38 -0600

    Update Login.js
    Created form and called database with axios and used md5 for pw

--- reactlogin/src/pages/Login.js
+++ reactlogin/src/pages/Login.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react'
 import '../css/Login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import md5 from 'md5';
+
+const baseUrl="http://localhost:3001/usuarios";
+
+
 class Login extends Component {
+
+  state={
+    form:{
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange=async e=>{
+    await this.setState({
+      form:{
+        ...this.state.from,
+        [e.target.name]: e.target.value
+      }
+    });
+    console.log(this.state.form)
+  }
+
+  iniciarSesion=async()=>{
+    await axios.get(baseUrl, {params: {username: this.state.form.username, password: md5(this.state.form.password)}})
+    .then(response=>{
+      console.log(response.data)
+    })
+    .catch(error=>{
+      console.log(error);
+    })
+  }
+
   render() {
     return (
       <div className='containerPrincipal'>
@@ -9,13 +43,13 @@ class Login extends Component {
           <div className='form-group'>
            <label>Usuario: </label>
            <br />
-           <input type="text" className='form-control' />
+           <input onChange={this.handleChange} name='username' type="text" className='form-control' />
            <br />
            <label>Contraseña: </label>
            <br />
-           <input type="password" className='form-control' />
+           <input onChange={this.handleChange} name='password' type="password" className='form-control' />
            <br />
-           <button className='btn btn-primary'>Iniciar Sesión</button>
+           <button onClick={()=> this.iniciarSesion()} className='btn btn-primary'>Iniciar Sesión</button>
           </div>
         </div>
       </div>
